9cd466981f763294e4da195bd7e07a374dec74aa
Bertalan Kovacs-Garai <bkovacs@chemaxon.com>
2024-11-26 16:07:36 +0100

    first commit

---
+++ src/main/java/com/chemaxon/Main.java
@@ -0,0 +1,11 @@
+package com.chemaxon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        var my = new MyClass(5);
+        System.out.println(my.getId());
+
+    }
+}
---
+++ src/main/java/com/chemaxon/MyClass.java
@@ -0,0 +1,13 @@
+package com.chemaxon;
+
+public class MyClass {
+    private int id;
+
+    public MyClass(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
