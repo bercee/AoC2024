373d6e51234183fa918568dba473dbeee4de986c
Bertalan Kovács-Garai <bertalan1990@gmail.com>
2024-11-30 23:12:20 +0100

    master: getting ready for 2024 day1

--- src/main/java/com/chemaxon/Solver.java
+++ src/main/java/com/chemaxon/Solver.java
@@ -1,8 +1,5 @@
 package com.chemaxon;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 
 public abstract class Solver {
---
+++ src/main/java/com/chemaxon/solvers/year2024/Day1.java
@@ -0,0 +1,21 @@
+package com.chemaxon.solvers.year2024;
+
+import com.chemaxon.Solver;
+
+import java.util.List;
+
+public class Day1 extends Solver {
+    public Day1(List<String> input) {
+        super(input);
+    }
+
+    @Override
+    public String solvePart1() {
+        return "";
+    }
+
+    @Override
+    public String solvePart2() {
+        return "";
+    }
+}
